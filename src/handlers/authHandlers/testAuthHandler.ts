b565ed7ce383916e0b44f3e63258621e1212622a
FabianKjellberg <120188031+FabianKjellberg@users.noreply.github.com>
2026-04-30 13:35:15 +0200

    add try cathc just in case

--- src/handlers/authHandlers/testAuthHandler.ts
+++ src/handlers/authHandlers/testAuthHandler.ts
@@ -1,7 +1,13 @@
 import { Context } from "hono";
 
 export const testAuthHandler = async (c: Context) => {
-  const userId = await c.get('userId');
-
-  return c.json({message : "authenticated correctly", userId: userId},200)
+  try {
+    const userId = await c.get('userId');
+    
+    return c.json({message : "authenticated correctly", userId: userId},200)
+  }
+  catch (error) {
+    console.error('Error testing auth:', error);
+    return c.json({ error: 'Internal server error' }, 500);
+  }
 }
